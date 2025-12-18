f7880dfb2ad3baf1c68eff519ebd26aac95b5e56
Dante Schrauwen <contact@danteschrauwen.be>
2025-12-18 17:32:04 +0100

    feat!: Complete Customer/Address DTOs for Peppol party lookup
    BREAKING CHANGES:
    - Customer constructor: `code`, `email`, `vatNumber` now optional
    - Customer required fields: only `name` and `address`
    
    Added:
    - TaxNumberType enum (BE enterprise, NL KvK, FR SIRENE)
    - ExtraIdentifier DTO for customer extra identifiers
    - Customer fields: peppolID, accountingCode, languageCode, invoiceEmail,
      contact, taxNumberType, taxNumber, glnNumber, extraIdentifiers
    - Address fields: streetBox, countrySubentity

---
+++ src/Data/Common/ExtraIdentifier.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Deinte\ScradaSdk\Data\Common;
+
+/**
+ * Extra identifier information for a party.
+ */
+final readonly class ExtraIdentifier
+{
+    public function __construct(
+        public ?string $scheme = null,
+        public ?string $value = null,
+    ) {
+    }
+
+    /**
+     * @param  array<string, mixed>  $data
+     */
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            scheme: isset($data['scheme']) && is_string($data['scheme']) ? $data['scheme'] : null,
+            value: isset($data['value']) && is_string($data['value']) ? $data['value'] : null,
+        );
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(): array
+    {
+        return array_filter([
+            'scheme' => $this->scheme,
+            'value' => $this->value,
+        ], static fn (mixed $v): bool => $v !== null);
+    }
+}
